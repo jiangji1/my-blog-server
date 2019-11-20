27afbc3bcc9df87a4a82543748ba2062b94b6e81
jiangji1 <774133054@qq.com>
2019-11-20 23:49:31 +0800

    列表查询取消权限检验

--- api/list.js
+++ api/list.js
@@ -2,11 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  const token = req.headers.token
-  if (!global.token[token]) {
-    res.send('无权限')
-    return
-  }
   const db = dbFn()
   const page = +req.query.page || 0
   const size = +req.query.size || 10
@@ -15,16 +10,9 @@ router.get('/', function(req, res, next) {
     if (err) {
       return res.send(err)
     }
-    console.log(data)
-    if (global.token[token]) {
-      res.send({
-        list: data,
-        power: 'all'
-      })
-      return
-    }
     res.send({
-      list: data
+      list: data,
+      power: global.token[token] && 'all',
     })
   })
 });
